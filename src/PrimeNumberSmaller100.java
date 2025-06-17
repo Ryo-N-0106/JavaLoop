5a356b942457e7ea5e0a891b544aa6af65c8dbab
Ryo-N-0106 <ntkn717416@gmail.com>
2025-06-17 21:08:37 +0700

    Số nguyên tố nhỏ hơn 100

---
+++ src/PrimeNumberSmaller100.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeNumberSmaller100 {
+    public static void main(String[] args) {
+        System.out.println("Các số nguyên tố nhỏ hơn 100: ");
+
+        for (int i = 2; i < 100; i++) {
+            if (isPrime(i)) {
+                System.out.println(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2){
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
